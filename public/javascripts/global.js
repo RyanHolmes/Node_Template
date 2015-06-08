c4ba3d73e27e47abb63b0eb02df4afa80bd7ccbc
Ryan Holmes <rholmes@Dylan-Harnesss-MacBook-Pro.local>
2015-06-08 11:25:41 -0400

    changes

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,9 +1,5 @@
 
-//Example Transition
+//called when about butto is clicked
 function aboutButton(){
-	$.get('/about', function(data) {
-		//data is the html of the about.jade page and the jquery html() function takes
-		//a string and renders it to the "Main"  -- <div id="Main"> </div>   
-		$('#Main').html(data);  
-    });
+	console.log("About");
 }
